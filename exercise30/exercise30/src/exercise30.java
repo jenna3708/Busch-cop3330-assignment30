d74547e911bdcaf0f4edd8e24bfaf1579a23fd51
jenna3708 <53448385+jenna3708@users.noreply.github.com>
2021-09-27 13:29:49 -0400

    Add files via upload

---
+++ exercise30/exercise30/src/exercise30.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 30 Solution
+ *  Copyright 2021 Jenna Busch
+ */
+
+import java.util.Scanner;
+
+public class exercise30 {
+
+
+    public static void main(String[] args) {
+
+        for (int i=1;i<=12;i++){
+            for (int j=1;j<=12;j++)
+                System.out.print("\t"+i*j);
+            System.out.println();
+        }
+
+    }
+
+}
